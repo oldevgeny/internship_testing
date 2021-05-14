11873d3c9f4f77aabd06826fc5ea4e8e45614d63
Evgeny Andreev <77259286+oldevgeny@users.noreply.github.com>
2021-05-14 04:31:54 +0300

    Delete tests.py

--- tests.py
+++
@@ -1,7 +0,0 @@
-# pip install pytest
-import pytest
-from validators import validate_output_order
-
-# @pytest.mark.parametrize()
-def test_validate_output_order():
-	assert validate_output_order('2') == 2
